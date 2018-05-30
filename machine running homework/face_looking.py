e063b27bd7bd2a55012f86e3a2f30ee261326de8
choi youngjin <qufehdquf9@naver.com>
2018-05-30 22:16:25 +0900

    add mlp,decisiontree

--- machine running homework/face_looking.py
+++ machine running homework/face_looking.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import fetch_lfw_people
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+rom sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 from sklearn import decomposition
 from sklearn import svm
@@ -14,19 +16,32 @@ pca.fit(X_train)
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 
-#rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=1)
-#clf=rf.fit(X_train, y_train)
-#y_pred1 = clf.predict(X_test)
-
-#knn = KNeighborsClassifier(n_neighbors=1, p=2,metric='minkowski')
-#knn_1 = knn.fit(X_train, y_train)
-#y_pred1 = knn_1.predict(X_test)
-
-#clf = svm.SVC(C=5., gamma=0.001)  # 분류 모델임.
-
-svc_1 = svm.SVC(kernel='linear').fit(X_train, y_train)
+#svm
+clf = svm.SVC(C=5., gamma=0.001)
+clf.fit(X_train, y_train)
+s = svm.SVC(kernel='linear').fit(X_train, y_train)
 svc_2 = svm.SVC(kernel='linear').fit(X_train_pca,y_train)
-y_pred1 = svc_1.predict(X_test)
-y_pred2 = svc_2.predict(X_test_pca)
-print(metrics.classification_report(y_test, y_pred1))
-print(metrics.classification_report(y_test, y_pred2))
+pred_svm = s.predict(X_test) #1.0
+pred_svm2 = svc_2.predict(X_test_pca)
+
+#knn
+knn = KNeighborsClassifier(n_neighbors=5, p=2,metric='minkowski')
+knn.fit(X_train, y_train)
+pred_knn = knn.predict(X_test)
+
+#random forest
+rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=0)
+rf.fit(X_train, y_train)
+pred_rf = rf.predict(X_test)
+
+#decision tree
+dt = DecisionTreeClassifier()
+dt.fit(X_train,y_train)
+pred_dt = dt.predict(X_test)
+
+#mlp
+mlp = MLPClassifier(solver='lbfgs', random_state=0)
+mlp.fit(X_train, y_train)
+pred_mlp = mlp.predict(X_test) #93%
+
+print(metrics.classification_report(y_test, pred_svm))
